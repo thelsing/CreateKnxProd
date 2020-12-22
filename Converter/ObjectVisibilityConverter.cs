25c2b751a70196c755fc2ad4c31056f01fdd3801
Thomas Kunze <thomas.kunze@gmx.com>
2020-12-22 20:27:51 +0100

    fix rowdetails

---
+++ Converter/ObjectVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using CreateKnxProd.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CreateKnxProd.Converter
+{
+    public class ObjectVisibilityConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Visibility))
+                return null;
+
+
+            if (value == null)
+                return Visibility.Collapsed;
+
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
