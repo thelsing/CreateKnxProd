875a5b03c9d522d35a75207e1d914e5804de7b90
Dennis Ostermann <dennis.ostermann@ise.de>
2021-11-23 08:01:08 +0100

    Ported to net6.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,4 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
+// [assembly: AssemblyVersion("1.2.*")]
+// Has been moved to csproj
